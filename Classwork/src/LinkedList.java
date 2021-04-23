e3016e588311d750a3a1a0d301e17d839873a933
Adithya <53436229+BlazedSamurai@users.noreply.github.com>
2021-04-23 19:24:31 +0800

    Update LinkedList.java

--- Classwork/src/LinkedList.java
+++ Classwork/src/LinkedList.java
@@ -31,23 +31,43 @@ public class LinkedList {
 
     public class linkedlist {
         Node start;
-
-        public linkedlist(int a) {
+        public linkedlist() {
             start.next = null;
         }
 
-        public void append(int element) {
-            Node temp = new Node(element);
-            temp = start;
-            while (temp.next != null){
-                temp = temp.next;
-            }
+    }
+    public void append(linkedlist a, int element) {
+        Node temp;
+        temp = a.start;
+        while (temp.next != null){
             temp = temp.next;
         }
+        Node appender = new Node(element);
+        appender = temp.next;
+    }
 
+    public void printList(linkedlist a){
+        Node temp2;
+        temp2 = a.start;
+
+        while (temp2.next!=null);
+        temp2=temp2.next;
+        System.out.print(temp.data + " ");
+    }
+
+    public void remove(linkedlist a, int element){
+        Node temp3;
+        temp3 = a.start;
+        while (temp3.next != null){
+            temp3=temp3.next;
+            if (temp3.data == element);
+
+        }
     }
 
     public static void main(String[] args){
+        linkedlist a = new linkedlist();
+        append(a, 5);
 
 
     }
